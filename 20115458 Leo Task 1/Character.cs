78afb6f5ebb01ad7b6c9d0f5ec1b6c09a39ae051
MikhailLeo <71314756+MikhailLeo@users.noreply.github.com>
2020-09-15 11:35:06 +0200

    Add files via upload

---
+++ 20115458 Leo Task 1/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20115458_Leo_Task_1
+{
+    class Character
+    {
+
+
+        public virtual void Attack()
+        {
+
+        }
+    }
+}
---
+++ 20115458 Leo Task 1/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20115458_Leo_Task_1
+{
+    abstract class Tile
+    {
+        protected  int X;
+        public int x
+        {
+            get { return X; }
+            set { X = value; }
+        }
+        protected  int Y;
+        public int y
+        {
+            get { return Y; }
+            set { Y = value; }
+        }
+    
+        /// <summary>
+        /// Q2.1
+        /// </summary>
+        public enum TileType
+        {
+            Hero = 0,
+            Enemy = 1,
+            Gold = 3,
+            Weapon = 4,
+        }
+        public Tile()
+        {
+            X = 0;
+            Y = 0;
+            
+            
+        }
+        class obstacle : Tile
+        {
+            public obstacle(int X, int Y, char symbol)
+            {
+                Console.WriteLine("X");
+            }
+        }
+     
+           
+     
+
+
+       
+      
+           
+
+        
+        
+             
+           
+    } 
+
+    
+    
+}
